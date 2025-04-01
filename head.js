3ff3d7dc8998a2c6e7833b25c9a40fc8961af20e
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-04-01 07:47:39 -0700

    contains assert equal function and a second function called head which returns the first element of the input array. new lines are test casses and head function

---
+++ head.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head =function(arr) {
+  let firstElement = arr[0];
+  return firstElement
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["lighthouse labs", 6, 7]), "Bootcamp");
+assertEqual(head([0, 6, 7]),  1);
+assertEqual(head([-1, 6, 7]),  "lighthouse labs");
+assertEqual(head([-1, 6, 7]),  -1);
+assertEqual(head([100, 6, 7]),  10);
+assertEqual(head([]),  10);
+assertEqual(head([100]),  10);
