6cf38e10c3fcf09a5dd2b5b1b703917a989cc401
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-04-01 21:00:13 -0700

    created a function called assertArraysEqual that calls eqArrays and creates outputs based on return boolean values. linted

---
+++ assertArraysEqual.js
@@ -0,0 +1,37 @@
+//fn eqArrays defined in eqArrays.js
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+//define function assertArraysEqual
+  //takes two perameters which are arrays
+  //outputs appropriate message (leverage assertEqual fn)
+    //fn calls eqArrays to check if arrays are identical
+      //output true:
+      //outpur false:
+    //takes above boolean and outputs messages
+      //if true...console.log()
+      //if false...console.log()
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Input Arrays Match: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Input Arrays DO NOT Match: ${array1} !== ${array2}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
