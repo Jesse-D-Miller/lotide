b18aef6374e24564c22c23328f7817879354cc4b
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-04-03 13:49:41 -0700

    function revised with GaryJ via Assistance request. revised for readability and simplification

--- eqObjects.js
+++ eqObjects.js
@@ -23,21 +23,47 @@ const eqArrays = function(arr1, arr2) {
 //output: true/fale equality
 //Check: same # of Keys && same values in same keys
 const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length === Object.keys(object2).length) {
-    for (const keys in object1) {
-      if (object1[keys] !== object2[keys]) {
-        // are both arrays and do they match VVV
-        if (!eqArrays(object1[keys], object2[keys] || !object1[keys].isArray || !object2[keys].isArray)){ 
-          return false;
-        }
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
 
+  if (keys1.length !== keys2.length){
+    return false; 
+  }
+  
+  for (key of keys1) {
+    const value1 = object1[key];
+    const value2 = object2[key];
+
+    if (Array.isArray(value1)) {
+      if (!eqArrays(value1, value2)){
+        return false;
+      }
+      continue;
+    }
+
+    if (value1 !== value2){
+      return false;
+    }
+
+  } 
+  return true;
 };
+//   return true;
+
+//     for (const keys in object1) {
+//       if (object1[keys] !== object2[keys]) {
+//         // are both arrays and do they match VVV
+//         if (!eqArrays(object1[keys], object2[keys] || !object1[keys].isArray || !object2[keys].isArray)){ 
+//           return false;
+//         }
+//       }
+//     }
+//     return true;
+//   } else {
+//     return false;
+//   }
+
+// };
 
 //test code
 
