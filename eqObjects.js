8d90542181b6e98350e4756d02db58731e8c2001
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-04-03 12:54:03 -0700

    working through the logic of eqObjects fn

--- eqObjects.js
+++ eqObjects.js
@@ -9,6 +9,16 @@ const assertEqual = function(actual, expected) {
 //output: true/fale equality
 //Check: same # of Keys && same values in same keys
 const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length === Object.keys(object2).length) {
+    for (const keys in object1) {
+      if (object1[keys] !== object2[keys]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
 
 };
 
