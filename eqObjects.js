86d2f59848143f6515b1541f351e5f93c6de978e
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-04-03 13:14:53 -0700

    added function eq arrays to handle arrays in objects. added call to eqArrays in eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -5,6 +5,20 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
 //input: 2x objects
 //output: true/fale equality
 //Check: same # of Keys && same values in same keys
@@ -12,7 +26,9 @@ const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length === Object.keys(object2).length) {
     for (const keys in object1) {
       if (object1[keys] !== object2[keys]) {
-        return false;
+        if (!eqArrays(object1[keys], object2[keys])){
+          return false;
+        }
       }
     }
     return true;
@@ -24,9 +40,9 @@ const eqObjects = function(object1, object2) {
 
 //test code
 
-const inputObject1 = {make: "Honda", colour: "red"};
-const inputObject2 = {make: "Honda", colour: "red"};
-eqObjects(inputObject1, inputObject2); //=>true
+const inputObject1 = {make: "Honda", colour: ["red", "black"]};
+const inputObject2 = {make: "Honda", colour: ["red", "black"]};
+assertEqual(eqObjects(inputObject1, inputObject2), true); //=>true
 
-const inputObject3 = {make: "Honda", colour: "red", model: "Civic"};
-eqObjects(inputObject1, inputObject3) //=>false
+const inputObject3 = {make: "Honda", colour: ["red", "black"], model: "Civic"};
+assertEqual(eqObjects(inputObject1, inputObject3), false) //=>false
