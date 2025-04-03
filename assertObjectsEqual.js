6a3c1d54ef70e3ec0ea9cc99ea53e51e4ca449d7
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-04-03 14:15:07 -0700

    added fns assertEqual, eqArrays, and eqObjects. implemented assertObjectsEqual. changed all functions to => notation

---
+++ assertObjectsEqual.js
@@ -0,0 +1,54 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const eqObjects = (object1, object2) => {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  
+  for (const key of keys1) {
+    const value1 = object1[key];
+    const value2 = object2[key];
+
+    if (Array.isArray(value1)) {
+      if (!eqArrays(value1, value2)) {
+        return false;
+      }
+      continue;
+    }
+
+    if (value1 !== value2) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+//input: 2x objects
+//output: console.log
+
+const assertObjectsEqual = (actual, expected) => {
+
+}
