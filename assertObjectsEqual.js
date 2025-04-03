930b3cec2702210a0b86c3a646b5ee0249ec6afe
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-04-03 14:43:35 -0700

    removed redundant fns

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,11 +1,3 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = (arr1, arr2) => {
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
@@ -50,5 +42,52 @@ const eqObjects = (object1, object2) => {
 //output: console.log
 
 const assertObjectsEqual = (actual, expected) => {
+  const inspect = require("util").inspect;
+  if (eqObjects(actual, expected)) {
+    return console.log(`✅✅✅ Input Objects Match: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    return console.log(`🛑🛑🛑 Input Objects DO NOT Match: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
 
-}
+const brandonSandersonBooks = {
+  author: "Brandon Sanderson",
+  totalBooksPublished: 50,  
+  mostFamousSeries: ["Mistborn", "The Stormlight Archive", "The Reckoners", "Warbreaker"],
+  firstPublishedBook: "Elantris",
+  cosmereUniverse: true,
+  averageBookLength: 500,  // Approximate pages
+  mostRecentBook: "Yumi and the Nightmare Painter",
+  genres: ["Fantasy", "Science Fiction", "Young Adult"],
+  famousCharacters: ["Kaladin", "Vin", "Kelsier", "Dalinar", "Shallan"],
+  upcomingBooks: ["Stormlight Archive 5", "Mistborn Era 3", "Dark One"]
+};
+
+const brandonSandersonBooksAlso = {
+  author: "Brandon Sanderson",
+  totalBooksPublished: 50,  
+  mostFamousSeries: ["Mistborn", "The Stormlight Archive", "The Reckoners", "Warbreaker"],
+  firstPublishedBook: "Elantris",
+  cosmereUniverse: true,
+  averageBookLength: 500,  // Approximate pages
+  mostRecentBook: "Yumi and the Nightmare Painter",
+  genres: ["Fantasy", "Science Fiction", "Young Adult"],
+  famousCharacters: ["Kaladin", "Vin", "Kelsier", "Dalinar", "Shallan"],
+  upcomingBooks: ["Stormlight Archive 5", "Mistborn Era 3", "Dark One"]
+};
+
+const notBrandonSandersonBooks = {
+  author: "Not Brandon Sanderson",
+  totalBooksPublished: 50,  
+  mostFamousSeries: ["Mistborn", "The Stormlight Archive", "The Reckoners", "Warbreaker"],
+  firstPublishedBook: "Elantris",
+  cosmereUniverse: true,
+  averageBookLength: 500,  // Approximate pages
+  mostRecentBook: "Yumi and the Nightmare Painter",
+  genres: ["Fantasy", "Science Fiction", "Young Adult"],
+  famousCharacters: ["Kaladin", "Vin", "Kelsier", "Dalinar", "Shallan"],
+  upcomingBooks: ["Stormlight Archive 5", "Mistborn Era 3", "Dark One"]
+};
+
+console.log(assertObjectsEqual(brandonSandersonBooks, brandonSandersonBooksAlso));
+console.log(assertObjectsEqual(brandonSandersonBooks, notBrandonSandersonBooks));
