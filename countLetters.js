30dfab396eef14cd767902ca0ef355280d497b63
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-04-02 18:58:04 -0700

    added assertEqual function. wrote pseudo

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//pseudo
+//
+//input: sentence as a STRING ""
+//output: return count of letters in string {}
+//declare fn countLetter
+//define outputObject locally
+//declare array
+//loop across array to put all letters in array
+//sort array alphabetically
+//loop accross array to count
+//  if next matches current
+//    count ++
+// if next !== current, add new property to object
+//return outputObject
+
+
+const countLetters = function(str) {
+
+};
