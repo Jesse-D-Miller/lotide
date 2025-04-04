b6fe0011bfef8302b5b8272d4b2a822d3ac17fab
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-04-04 08:41:25 -0700

    i wrote out the function and it works to return an array of the first character in each string element

--- map.js
+++ map.js
@@ -12,10 +12,43 @@ const stormlightBooks = [
   "Dawnshard"
 ];
 
-const map = function() {
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Input Arrays Match: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Input Arrays DO NOT Match: ${array1} !== ${array2}`);
+  }
+};
+
+const map = function(array, callback) {
+  
+  const results = [];
+
+  for (let book of array) {
+    results.push(callback(book));
+  }
+
+  return results;
 
 };
 
 const mapAString = map(stormlightBooks, (book) => book[0]);
-console.log(mapAString);
 
+
+
+// test cases
+let expextedString = [ 'T', 'W', 'O', 'R', 'D' ]
+assertArraysEqual(mapAString, expextedString);
