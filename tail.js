ccf0f5ef0798a809d0373b3404aeeb02fce5887a
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-04-07 18:34:38 -0700

    edited tail fn as it did not pass assertions as it was written

--- tail.js
+++ tail.js
@@ -1,6 +1,9 @@
 //this function takes an array as an input and returns an array of all the elements after the first as another array
 
 const tail = function(arr) {
+  if (arr.length === 0 || arr.length === 1) {
+    return undefined;
+  }
   return arr.slice(1); //returns a slice of this array starting at [1]
 };
 
