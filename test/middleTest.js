db87a6d9c77ee6744e5e3e52c97e8d63b332de01
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-04-07 18:48:19 -0700

    refactor test case to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,63 @@
-const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
+// const middle = require("../middle");
+// const assertArraysEqual = require("../assertArraysEqual");
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle(["cat", true, "aunt"]), [true]);
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+// assertArraysEqual(middle([]), []);
+// assertArraysEqual(middle([1]), []);
+// assertArraysEqual(middle([1, 2]), []);
+// assertArraysEqual(middle(["cat", true, "aunt"]), [true]);
+
+const assert   = require('chai').assert;
+const middle     = require('../middle');
+const eqArrays = require("../eqArrays");
+let inputArray = [];
+expectedOutput = [];
+
+describe("#middle", () => {
+
+  it("should return [2] when given the input [1, 2, 3]", () => {
+    inputArray = [1, 2, 3];
+    expectedOutput = [2];
+    assert(eqArrays(middle(inputArray), expectedOutput));
+  });
+
+  it("should return [3] when given the input [1, 2, 3, 4, 5]", () => {
+    inputArray = [1, 2, 3, 4, 5];
+    expectedOutput = [3];
+    assert(eqArrays(middle(inputArray), expectedOutput));
+  });
+
+  it("should return [2, 3] when given the input [1, 2, 3, 4]", () => {
+    inputArray = [1, 2, 3, 4];
+    expectedOutput = [2, 3];
+    assert(eqArrays(middle(inputArray), expectedOutput));
+  });
+
+  it("should return [] when given the input []", () => {
+    inputArray = [];
+    expectedOutput = [];
+    assert(eqArrays(middle(inputArray), expectedOutput));
+  });
+
+  it("should return [] when given the input [1]", () => {
+    inputArray = [1];
+    expectedOutput = [];
+    assert(eqArrays(middle(inputArray), expectedOutput));
+  });
+
+  it("should return [] when given the input [1, 2]", () => {
+    inputArray = [1, 2];
+    expectedOutput = [];
+    assert(eqArrays(middle(inputArray), expectedOutput));
+  });
+
+  it("should return [true] when given the input ['cat', true 'aunt']", () => {
+    inputArray = ["cat", true, "aunt"];
+    expectedOutput = [true];
+    assert(eqArrays(middle(inputArray), expectedOutput));
+  });
+
+});
